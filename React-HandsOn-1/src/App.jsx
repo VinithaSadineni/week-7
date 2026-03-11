06ae9f5e65dbd6cf506c6f5b01b94c9f70d736f9
VinithaSadineni <vinithasadineni252@gmail.com>
2026-03-11 15:09:05 +0530

    initial commit

---
+++ React-HandsOn-1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+// product listing components
+import Products from './Components/Products.jsx'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Products />
+    </div>
+  )
+}
+
+export default App
